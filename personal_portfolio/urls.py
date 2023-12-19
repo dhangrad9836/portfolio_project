4aef2b3141f0843cc9e078a1e5a935d9c3af4d4c
Darren Dhanpat <darren.dhanpat@yahoo.com>
2023-12-19 08:53:47 -0500

    adding blog and blog model

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -15,14 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from portfolio import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home)
+    path('', views.home, name='home'),
+    path('blog/', include('blog.urls'))
 ]
 
 #we need to specify what the media url and the media root is going to be.
